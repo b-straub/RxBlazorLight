579b77b0e4c8a4c12b0a48c3fb14f4b908a3c236
Bernhard Straub <bstraubka@hotmail.com>
2024-03-24 18:50:01 +0100

    RX State all new State handling
    Add state subscriber for dedicated performance sensitive updating

---
+++ RxBlazorLightCore/Component/RxBLStateSubscriber.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Reactive.Linq;
+
+namespace RxBlazorLightCore;
+
+public partial class RxBLStateSubscriber<T> : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public required T Service { get; init; }
+
+    [Parameter, EditorRequired]
+    public required Guid[] IDs { get; init; }
+
+    [Parameter]
+    public required RenderFragment ChildContent { get; init; }
+
+    [Parameter]
+    public double SampleRateMS { get; set; } = 100;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        Service
+            .Sample(TimeSpan.FromMilliseconds(SampleRateMS))
+            .Where(cr => IDs.Any(i => i == cr.StateID))
+            .Subscribe(cr =>
+            {
+                OnServiceStateHasChanged(cr);
+                InvokeAsync(StateHasChanged);
+            });
+    }
+
+    protected virtual void OnServiceStateHasChanged(ServiceChangeReason cr)
+    {
+    }
+}

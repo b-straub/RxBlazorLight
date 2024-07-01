6b6849d8d87eebe8ba0d78a315a206c884d436e5
Bernhard Straub <bsoft@mandel-straub.de>
2024-07-01 15:55:15 +0200

    Improve DialogResult extension
    Add null check

--- RxMudBlazorLight/Extensions/RxMudBlazorLightExtensionsExtern.cs
+++ RxMudBlazorLight/Extensions/RxMudBlazorLightExtensionsExtern.cs
@@ -1,10 +1,11 @@
-﻿using MudBlazor;
+﻿using System.Diagnostics.CodeAnalysis;
+using MudBlazor;
 
 namespace RxMudBlazorLight.Extensions
 {
     public static class RxMudBlazorLightExtensionsExtern
     {
-        public static bool OK(this DialogResult? result)
+        public static bool OK([NotNullWhen(true)] this DialogResult? result)
         {
             return result is not null && !result.Canceled;
         }

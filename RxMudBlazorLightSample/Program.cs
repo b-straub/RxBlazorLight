dabd84da6b8bf81500427c3f8ba0cf4555590853
Bernhard Straub <bsoft@mandel-straub.de>
2025-02-04 18:50:33 +0100

    Fix testing
    Update tests to MudBlazor 8.x

--- RxMudBlazorLightSample/Program.cs
+++ RxMudBlazorLightSample/Program.cs
@@ -11,10 +11,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices(options =>
-{
-    options.PopoverOptions.ThrowOnDuplicateProvider = false;
-});
+builder.Services.AddMudServices();
 builder.Services.AddSingleton(sp => new TestService(sp));
 builder.Services.AddSingleton<TimerService>();
 builder.Services.AddScoped<StateService>();
